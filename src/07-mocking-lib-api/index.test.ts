99e35188890ce97355f2902326e0b665cda2affb
5tronciy <github@5tronciy.org>
2025-05-11 00:13:12 +0200

    feat: mock axios and lodash api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,69 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+
+jest.mock('lodash', () => {
+  const originalModule = jest.requireActual('lodash');
+  return {
+    ...originalModule,
+    throttle: jest.fn((fn) => fn),
+  };
+});
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (axios.create as jest.Mock).mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: {} }),
+    });
+  });
+
+  afterAll(() => {
+    jest.unmock('axios');
+    jest.unmock('lodash');
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const relativePath = '/users/1';
+
+    await throttledGetDataFromApi(relativePath);
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const mockAxiosInstance = {
+      get: jest.fn().mockResolvedValue({ data: {} }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValue(mockAxiosInstance);
+
+    await throttledGetDataFromApi(relativePath);
+
+    expect(mockAxiosInstance.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const relativePath = '/comments/1';
+    const mockResponseData = {
+      id: 1,
+      title: 'Test Post',
+      body: 'This is a test post',
+    };
+
+    const mockAxiosInstance = {
+      get: jest.fn().mockResolvedValue({ data: mockResponseData }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValue(mockAxiosInstance);
+
+    const result = await throttledGetDataFromApi(relativePath);
+
+    expect(result).toEqual(mockResponseData);
   });
 });
